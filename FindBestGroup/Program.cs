4e826988cdc3b87490d0f2f3a784b268c51f5ad5
kcseason <309084525@qq.com>
2019-02-25 14:50:46 +0800

    添加项目文件。

---
+++ FindBestGroup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindBestGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
